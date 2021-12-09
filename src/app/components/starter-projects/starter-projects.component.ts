274afa451c99936cb9adc1850273c4c4a16ee006
Davuttrg <davut.0773@gmail.com>
2021-12-09 11:10:26 +0300

    starter projects css & animation

--- src/app/components/starter-projects/starter-projects.component.ts
+++ src/app/components/starter-projects/starter-projects.component.ts
@@ -9,6 +9,9 @@ import { Router } from '@angular/router';
 export class StarterProjectsComponent implements OnInit {
   @Output() clickedProject: EventEmitter<any> = new EventEmitter();
   constructor(private _router: Router) {}
+
+  projects = [];
+
   gotoProject(route) {
     // this._router.navigate([route]);
     this._router.navigateByUrl(route, {
@@ -16,5 +19,39 @@ export class StarterProjectsComponent implements OnInit {
     });
     this.clickedProject.emit();
   }
-  ngOnInit() {}
+  ngOnInit() {
+    this.projects = [
+      {
+        name: 'Food Delivery',
+        route: '/food-delivery/home',
+        icon: 'fast-food-outline',
+      },
+      {
+        name: 'Fitness Application',
+        route: '/fitness/home',
+        icon: 'barbell-outline',
+      },
+      {
+        name: 'E-Commerce Application',
+        route: '/e-commerce',
+        icon: 'bag-handle-outline',
+      },
+      {
+        name: 'Social Media',
+        route: '/social-media',
+        icon: 'thumbs-up-outline',
+      },
+      {
+        name: 'Music Streaming',
+        route: '/music-streaming',
+        icon: 'musical-notes-outline',
+      },
+      { name: 'Chat', route: '/chat', icon: 'chatbox-ellipses-outline' },
+      { name: 'Job Portal', route: '/job-portal', icon: 'filter-outline' },
+      { name: 'Portfolio', route: '/portfolio', icon: 'newspaper-outline' },
+      { name: 'Store Listing', route: '/store-listing', icon: 'storefront-outline' },
+      { name: 'Appointment', route: '/appointment', icon: 'calendar-outline' },
+      { name: 'Forum', route: '/forum', icon: 'text-outline' },
+    ];
+  }
 }
