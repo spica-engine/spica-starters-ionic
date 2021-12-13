d21a4760c97f45cbd5b798466952044f2b5286ab
idriska <idriscumali@gmail.com>
2021-12-13 13:12:52 +0300

    Forum - bucket.ts refactored

--- src/app/forum/services/bucket.ts
+++ src/app/forum/services/bucket.ts
@@ -6,7 +6,7 @@ import * as Bucket from '@spica-devkit/bucket';
 export function initialize(
   ...initOptions: Parameters<typeof Bucket.initialize>
 ) {
-  initOptions[0].publicUrl = 'https://asset-test-9ef6b.hq.spicaengine.com/api/';
+  initOptions[0].publicUrl = 'https://spica-starters-7229b.hq.spicaengine.com/api';
   Bucket.initialize(...initOptions);
 }
 
@@ -16,7 +16,69 @@ type getAllArgs = Rest<Parameters<typeof Bucket.data.getAll>>;
 type realtimeGetArgs = Rest<Parameters<typeof Bucket.data.realtime.get>>;
 type realtimeGetAllArgs = Rest<Parameters<typeof Bucket.data.realtime.getAll>>;
 type id = { _id: string };
-
+export interface Category{
+  _id?: string;
+  name?: string;
+  titles?: (Title & id | string)[];
+  surveys?: (Survey & id | string)[];
+  popularity?: number;
+}
+export namespace category {
+  const BUCKET_ID = '61b70e5fb7f4a7002e04c79d';
+    export function get (...args: getArgs) {
+      return Bucket.data.get<Category & id>(BUCKET_ID, ...args);
+    };
+    export function getAll (...args: getAllArgs) {
+      return Bucket.data.getAll<Category & id>(BUCKET_ID, ...args);
+    };
+    export function insert (document: Omit<Category, "_id">) {
+      ['titles','surveys'].forEach((field) => {
+        if (typeof document[field] == 'object') {
+          document[field] = Array.isArray(document[field])
+            ? document[field].map((v) => v._id ? v._id : v)
+            : document[field]._id;
+        }
+      });
+      return Bucket.data.insert(BUCKET_ID, document);
+    };
+    export function update (document: Category & id) {
+      ['titles','surveys'].forEach((field) => {
+        if (typeof document[field] == 'object') {
+          document[field] = Array.isArray(document[field])
+            ? document[field].map((v) => v._id ? v._id : v)
+            : document[field]._id;
+        }
+      });
+      return Bucket.data.update(
+        BUCKET_ID,
+        document._id,
+        document
+      );
+    };  
+    export function patch (
+      document: Partial<Category> & id
+    ) {
+      ['titles','surveys'].forEach((field) => {
+        if (typeof document[field] == 'object') {
+          document[field] = Array.isArray(document[field])
+            ? document[field].map((v) => v._id ? v._id : v)
+            : document[field]._id;
+        }
+      });
+      return Bucket.data.patch(BUCKET_ID, document._id, document);
+    };  
+    export function remove (documentId: string) {
+      return Bucket.data.remove(BUCKET_ID, documentId);
+    };
+  export namespace realtime {
+      export function get (...args: realtimeGetArgs) {
+        return Bucket.data.realtime.get<Category & id>(BUCKET_ID, ...args);
+      };
+      export function getAll (...args: realtimeGetAllArgs) {
+        return Bucket.data.realtime.getAll<Category & id>(BUCKET_ID, ...args);
+      };
+  }
+}
 
 export interface User{
   _id?: string;
@@ -32,7 +94,7 @@ export interface User{
   identity_id?: string;
 }
 export namespace user {
-  const BUCKET_ID = '618cd908c76489002e9b60f3';
+  const BUCKET_ID = '61b70e5eb7f4a7002e04c79a';
     export function get (...args: getArgs) {
       return Bucket.data.get<User & id>(BUCKET_ID, ...args);
     };
@@ -43,7 +105,7 @@ export namespace user {
       ['followings','followers'].forEach((field) => {
         if (typeof document[field] == 'object') {
           document[field] = Array.isArray(document[field])
-            ? document[field].map((v) =>  v._id ? v._id : v)
+            ? document[field].map((v) => v._id ? v._id : v)
             : document[field]._id;
         }
       });
@@ -69,7 +131,7 @@ export namespace user {
       ['followings','followers'].forEach((field) => {
         if (typeof document[field] == 'object') {
           document[field] = Array.isArray(document[field])
-            ? document[field].map((v) =>  v._id ? v._id : v)
+            ? document[field].map((v) => v._id ? v._id : v)
             : document[field]._id;
         }
       });
@@ -88,70 +150,6 @@ export namespace user {
   }
 }
 
-export interface Category{
-  _id?: string;
-  name?: string;
-  titles?: (Title & id | string)[];
-  surveys?: (Survey & id | string)[];
-  popularity?: number;
-}
-export namespace category {
-  const BUCKET_ID = '618cd90ac76489002e9b60f6';
-    export function get (...args: getArgs) {
-      return Bucket.data.get<Category & id>(BUCKET_ID, ...args);
-    };
-    export function getAll (...args: getAllArgs) {
-      return Bucket.data.getAll<Category & id>(BUCKET_ID, ...args);
-    };
-    export function insert (document: Omit<Category, "_id">) {
-      ['titles','surveys'].forEach((field) => {
-        if (typeof document[field] == 'object') {
-          document[field] = Array.isArray(document[field])
-            ? document[field].map((v) =>  v._id ? v._id : v)
-            : document[field]._id;
-        }
-      });
-      return Bucket.data.insert(BUCKET_ID, document);
-    };
-    export function update (document: Category & id) {
-      ['titles','surveys'].forEach((field) => {
-        if (typeof document[field] == 'object') {
-          document[field] = Array.isArray(document[field])
-            ? document[field].map((v) =>  v._id ? v._id : v)
-            : document[field]._id;
-        }
-      });
-      return Bucket.data.update(
-        BUCKET_ID,
-        document._id,
-        document
-      );
-    };  
-    export function patch (
-      document: Partial<Category> & id
-    ) {
-      ['titles','surveys'].forEach((field) => {
-        if (typeof document[field] == 'object') {
-          document[field] = Array.isArray(document[field])
-            ? document[field].map((v) =>  v._id ? v._id : v)
-            : document[field]._id;
-        }
-      });
-      return Bucket.data.patch(BUCKET_ID, document._id, document);
-    };  
-    export function remove (documentId: string) {
-      return Bucket.data.remove(BUCKET_ID, documentId);
-    };
-  export namespace realtime {
-      export function get (...args: realtimeGetArgs) {
-        return Bucket.data.realtime.get<Category & id>(BUCKET_ID, ...args);
-      };
-      export function getAll (...args: realtimeGetAllArgs) {
-        return Bucket.data.realtime.getAll<Category & id>(BUCKET_ID, ...args);
-      };
-  }
-}
-
 export interface Title{
   _id?: string;
   name?: string;
@@ -161,7 +159,7 @@ export interface Title{
   created_at?: Date | string;
 }
 export namespace title {
-  const BUCKET_ID = '618cd90bc76489002e9b60f9';
+  const BUCKET_ID = '61b70e61b7f4a7002e04c7a0';
     export function get (...args: getArgs) {
       return Bucket.data.get<Title & id>(BUCKET_ID, ...args);
     };
@@ -172,7 +170,7 @@ export namespace title {
       ['user','comments'].forEach((field) => {
         if (typeof document[field] == 'object') {
           document[field] = Array.isArray(document[field])
-            ? document[field].map((v) =>  v._id ? v._id : v)
+            ? document[field].map((v) => v._id ? v._id : v)
             : document[field]._id;
         }
       });
@@ -182,7 +180,7 @@ export namespace title {
       ['user','comments'].forEach((field) => {
         if (typeof document[field] == 'object') {
           document[field] = Array.isArray(document[field])
-            ? document[field].map((v) =>  v._id ? v._id : v)
+            ? document[field].map((v) => v._id ? v._id : v)
             : document[field]._id;
         }
       });
@@ -219,18 +217,18 @@ export namespace title {
 
 export interface Comment{
   _id?: string;
+  content?: string;
   date?: Date | string;
   user?: (User & id | string);
   likes?: (User & id | string)[];
   dislikes?: (User & id | string)[];
   images?: string[];
-  content?: string;
-  comments?: (Comment & id | string)[];
   is_post?: boolean;
+  comments?: (Comment & id | string)[];
   category?: (Category & id | string)[];
 }
 export namespace comment {
-  const BUCKET_ID = '618cd90dc76489002e9b60fc';
+  const BUCKET_ID = '61b70e62b7f4a7002e04c7a3';
     export function get (...args: getArgs) {
       return Bucket.data.get<Comment & id>(BUCKET_ID, ...args);
     };
@@ -238,20 +236,20 @@ export namespace comment {
       return Bucket.data.getAll<Comment & id>(BUCKET_ID, ...args);
     };
     export function insert (document: Omit<Comment, "_id">) {
-      ['user','likes','dislikes','comments'].forEach((field) => {
+      ['user','likes','dislikes','comments','category'].forEach((field) => {
         if (typeof document[field] == 'object') {
           document[field] = Array.isArray(document[field])
-            ? document[field].map((v) =>  v._id ? v._id : v)
+            ? document[field].map((v) => v._id ? v._id : v)
             : document[field]._id;
         }
       });
       return Bucket.data.insert(BUCKET_ID, document);
     };
     export function update (document: Comment & id) {
-      ['user','likes','dislikes','comments'].forEach((field) => {
+      ['user','likes','dislikes','comments','category'].forEach((field) => {
         if (typeof document[field] == 'object') {
           document[field] = Array.isArray(document[field])
-            ? document[field].map((v) =>  v._id ? v._id : v)
+            ? document[field].map((v) => v._id ? v._id : v)
             : document[field]._id;
         }
       });
@@ -264,7 +262,7 @@ export namespace comment {
     export function patch (
       document: Partial<Comment> & id
     ) {
-      ['user','likes','dislikes','comments'].forEach((field) => {
+      ['user','likes','dislikes','comments','category'].forEach((field) => {
         if (typeof document[field] == 'object') {
           document[field] = Array.isArray(document[field])
             ? document[field].map((v) => v._id ? v._id : v)
@@ -292,12 +290,10 @@ export interface Survey{
   title?: string;
   selections?: {
   option?: string;
-  total_votes?: number;
-  voter?: (User & id | string)[];
-}[];
+  voter?: (User & id | string)[];}[];
 }
 export namespace survey {
-  const BUCKET_ID = '618cd90ec76489002e9b60ff';
+  const BUCKET_ID = '61b70e64b7f4a7002e04c7a6';
     export function get (...args: getArgs) {
       return Bucket.data.get<Survey & id>(BUCKET_ID, ...args);
     };
@@ -318,7 +314,7 @@ export namespace survey {
       ['user'].forEach((field) => {
         if (typeof document[field] == 'object') {
           document[field] = Array.isArray(document[field])
-            ? document[field].map((v) =>  v._id ? v._id : v)
+            ? document[field].map((v) => v._id ? v._id : v)
             : document[field]._id;
         }
       });
@@ -334,7 +330,7 @@ export namespace survey {
       ['user'].forEach((field) => {
         if (typeof document[field] == 'object') {
           document[field] = Array.isArray(document[field])
-            ? document[field].map((v) =>  v._id ? v._id : v)
+            ? document[field].map((v) => v._id ? v._id : v)
             : document[field]._id;
         }
       });
