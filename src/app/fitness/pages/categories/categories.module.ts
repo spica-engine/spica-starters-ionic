a2894a85676123bfdcf41024696e8c96cb477631
Davuttrg <davut.0773@gmail.com>
2021-12-23 12:55:41 +0300

    fitness some ui changes

---
+++ src/app/fitness/pages/categories/categories.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { CategoriesPageRoutingModule } from './categories-routing.module';
+
+import { CategoriesPage } from './categories.page';
+import { SpicaComponentsModule } from 'src/app/components/components.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    CategoriesPageRoutingModule,
+    SpicaComponentsModule
+  ],
+  declarations: [CategoriesPage]
+})
+export class CategoriesPageModule {}
